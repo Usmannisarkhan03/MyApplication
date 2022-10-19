89a2a8daf7e6145dd8aa905689c72f2cd8785e44
Usman <usmannisarkhan03@gmail.com>
2022-10-19 22:19:40 +0500

    dev comit

--- app/src/main/java/com/app/myapplication/MainActivity.kt
+++ app/src/main/java/com/app/myapplication/MainActivity.kt
@@ -5,6 +5,17 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+
+
+
+
+
+
+
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
